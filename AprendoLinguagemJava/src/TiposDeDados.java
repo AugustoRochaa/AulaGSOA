4714fa46a1bd259c7811c5741f1be5f2a8758526
AugustoRochaa <augustorf02@gmail.com>
2018-09-20 22:37:57 -0300

    Adicionei o arq java

---
+++ AprendoLinguagemJava/src/TiposDeDados.java
@@ -0,0 +1,18 @@
+
+public class TiposDeDados {
+
+    public static void main(String[] args) {
+        //textos
+        char letra= 'a';
+        String texto= "texto";
+        
+        //lógico
+        boolean valor= true;
+        
+        //numérico
+        int inteiro = 0;
+        long inteiroDobrado= 20;
+        float real= 10.3f;
+        double realDobrado=20.0;
+    }
+}
